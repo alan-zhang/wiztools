0407b723f1adabe9d9723f1269578e8171a483a0
subwiz <subwiz@86d51882-3021-11de-ba44-d58fe8a9ce33>
2009-11-20 16:25:40 +0000

    logic added, but still not working

--- rackspacecloudfiles-cli/trunk/src/org/wiztools/rackspacecloudfiles/Main.java
+++ rackspacecloudfiles-cli/trunk/src/org/wiztools/rackspacecloudfiles/Main.java
@@ -1,12 +1,49 @@
 package org.wiztools.rackspacecloudfiles;
 
 import com.rackspacecloud.client.cloudfiles.FilesClient;
+import com.rackspacecloud.client.cloudfiles.FilesContainer;
+import com.rackspacecloud.client.cloudfiles.FilesObject;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 /**
  *
  * @author subwiz
  */
 public class Main {
+
+    private static void listContainers(FilesClient client) throws IOException{
+        List<FilesContainer> containers =  client.listContainers();
+        for(FilesContainer container: containers){
+            System.out.println(container.getName());
+        }
+    }
+
+    private static void listOrCreateContainer(FilesClient client, String containerName) throws IOException{
+        if(client.containerExists(containerName)){
+            List<FilesObject> files = client.listObjects(containerName);
+            for(FilesObject file: files){
+                System.out.println(file.getName());
+            }
+        }
+        else{
+            client.createContainer(containerName);
+            System.out.println("Successfully created container: " + containerName);
+        }
+    }
+
+    private static void uploadFileToContainer(FilesClient client,
+                String containerName,
+                File fileToUpload) throws IOException{
+        if(!client.containerExists(containerName)){
+            client.createContainer(containerName);
+            System.out.println("Successfully created container: " + containerName);
+        }
+        boolean result = client.storeObject(containerName, fileToUpload, containerName);
+        System.out.println("File upload " + fileToUpload + " result: " + result);
+    }
+
     public static void main(String[] args) throws Exception{
         if(args.length < 2){
             System.err.println("Command-line arguments:");
@@ -21,7 +58,19 @@ public class Main {
         final String apiKey = args[1];
 
         FilesClient client = new FilesClient(username, apiKey);
+        client.login();
 
-        
+        switch(args.length){
+            case 2:
+                listContainers(client);
+                break;
+            case 3:
+                listOrCreateContainer(client, args[2]);
+                break;
+            case 4:
+                uploadFileToContainer(client, args[2] , new File(args[3]));
+                break;
+        }
+
     }
 }
