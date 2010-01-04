f44a6dc9d338683ec6ce5a99d77edd2e3dcfc389
subwiz <subwiz@86d51882-3021-11de-ba44-d58fe8a9ce33>
2010-01-04 10:24:43 +0000

    [maven-release-plugin]  copy for tag uuidgen-1.0

---
+++ uuidgen/tags/uuidgen-1.0/src/main/java/org/wiztools/util/uuidgen/Main.java
@@ -0,0 +1,13 @@
+package org.wiztools.util.uuidgen;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author subwiz
+ */
+public class Main {
+    public static void main(String[] arg){
+        System.out.println(UUID.randomUUID());
+    }
+}
