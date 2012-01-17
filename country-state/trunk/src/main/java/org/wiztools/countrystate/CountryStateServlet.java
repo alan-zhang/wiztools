61e2a3655b31681725ca27dc9234cce00649ab60
subwiz <subwiz@86d51882-3021-11de-ba44-d58fe8a9ce33>
2012-01-17 10:56:08 +0000

    initial import of the country state webapp

---
+++ country-state/trunk/src/main/java/org/wiztools/countrystate/CountryStateServlet.java
@@ -0,0 +1,40 @@
+package org.wiztools.countrystate;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author subwiz
+ */
+public class CountryStateServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
---
+++ country-state/trunk/src/main/java/org/wiztools/countrystate/StateBean.java
@@ -0,0 +1,12 @@
+package org.wiztools.countrystate;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author subwiz
+ */
+@XmlRootElement
+public class StateBean extends PlaceBean {
+    
+}
