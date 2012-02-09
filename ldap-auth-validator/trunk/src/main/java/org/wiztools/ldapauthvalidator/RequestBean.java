9e00d41942e88bd9d5653324c7aacd0e2c287dd9
subwiz <subwiz@86d51882-3021-11de-ba44-d58fe8a9ce33>
2012-02-09 07:01:38 +0000

    refactoring package and class names.

---
+++ ldap-auth-validator/trunk/src/main/java/org/wiztools/ldapauthvalidator/RequestBean.java
@@ -0,0 +1,58 @@
+package org.wiztools.ldapauthvalidator;
+
+/**
+ *
+ * @author subwiz
+ */
+public class RequestBean {
+    private String ldapUrl;
+    private String baseDN;
+    private String userCN;
+    private String password;
+
+    public String getBaseDN() {
+        return baseDN;
+    }
+
+    public void setBaseDN(String baseDN) {
+        this.baseDN = baseDN;
+    }
+
+    public String getFqdnLdapUrl() {
+        StringBuilder sb = new StringBuilder(ldapUrl);
+        if(!ldapUrl.endsWith("/")) {
+            sb.append("/");
+        }
+        sb.append(baseDN);
+        return sb.toString();
+    }
+
+    public void setLdapUrl(String ldapUrl) {
+        this.ldapUrl = ldapUrl;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserCN() {
+        return userCN;
+    }
+
+    public void setUserCN(String userCN) {
+        this.userCN = userCN;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{");
+        sb.append("ldapUrl=").append(ldapUrl).append("; ")
+                .append("baseDN=").append(baseDN).append("; ")
+                .append("userCN=").append(userCN).append("}");
+        return sb.toString();
+    }
+}
